0ffc0400163118e2fa517ed5999b5bcfa9bce982
maximofn <maximofn@gmail.com>
2023-11-18 04:02:28 +0100

    Changes into speech_to_text.py
    Remove audio file at finish

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,6 +1,8 @@
 import whisper
+import os
 
 def SST(audio, whisper_model="large-v3", device='cuda'):
     model = whisper.load_model(whisper_model, device=device)
     result = model.transcribe(audio, word_timestamps=True)
+    os.remove(audio)
     return result
