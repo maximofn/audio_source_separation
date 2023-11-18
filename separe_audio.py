a8e67823dba7addaa568742b44b1a86ea8377b7c
maximofn <maximofn@gmail.com>
2023-11-18 04:00:54 +0100

    Changes into separe_audio.py
    Add device as input parameter
    
    Remove audio file at finish

--- separe_audio.py
+++ separe_audio.py
@@ -1,5 +1,6 @@
 from speechbrain.pretrained import SepformerSeparation as separator
 import torchaudio
+import os
 
 pretrained_models = {
     "sepformer-wsj02mix": {
@@ -19,12 +20,20 @@ pretrained_models = {
     },
 }
 
-def separate_audio(audio_file, pretrained_model_name="sepformer-libri2mix"):
+def separate_audio(audio_file, pretrained_model_name="sepformer-libri2mix", device="cuda"):
     audio, extension = audio_file.split(".")
     audio_name = audio.split("/")[-1]
     pretrained_model = pretrained_models[pretrained_model_name]
-    model = separator.from_hparams(source=pretrained_model["source"], savedir=pretrained_model["savedir"], run_opts={"device":"cuda"})
+    model = separator.from_hparams(source=pretrained_model["source"], savedir=pretrained_model["savedir"], run_opts={"device":device})
     estimated_sources = model.separate_file(path=audio_file)
 
+    separed_audios = []
     for i in range(0, estimated_sources.shape[2]):
-        torchaudio.save(f"{audio_name}_source_{i}.{extension}", estimated_sources[:, :, i].detach().cpu(), pretrained_model["sample_rate"])
+        separed_audio = f"{audio_name}_source_{i}.{extension}"
+        torchaudio.save(separed_audio, estimated_sources[:, :, i].detach().cpu(), pretrained_model["sample_rate"])
+        separed_audios.append(separed_audio)
+    
+    # remove audio_file
+    os.remove(audio_file)
+    
+    return separed_audios
